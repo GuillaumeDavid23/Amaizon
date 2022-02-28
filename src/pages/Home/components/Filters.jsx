19e74269c82055ac98c531d1e8af1601190112a0
Vincent Mancheron <vmancheron@yahoo.fr>
2022-02-28 15:17:58 +0100

    28/02/22 15:17 Start Homepage

---
+++ src/pages/Home/components/Filters.jsx
@@ -0,0 +1,18 @@
+import Input from '../../../templates/Input'
+
+const Filters = () => {
+	const handleSearch = () => {}
+
+	return (
+		<div className="d-flex justify-content-around">
+			<Input id="" type="text" onBlur={handleSearch} />
+			<Input id="" type="text" onBlur={handleSearch} />
+			<Input id="" type="text" onBlur={handleSearch} />
+			<Input id="" type="text" onBlur={handleSearch} />
+			<Input id="" type="text" onBlur={handleSearch} />
+			<Input id="" type="text" onBlur={handleSearch} />
+		</div>
+	)
+}
+
+export default Filters
