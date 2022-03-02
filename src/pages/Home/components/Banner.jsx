01856b2bca94d124da76cd4bd2eb930c42bf9b60
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-03-02 16:40:51 +0100

    link on contact btn banner

--- src/pages/Home/components/Banner.jsx
+++ src/pages/Home/components/Banner.jsx
@@ -1,4 +1,6 @@
 import BtnGeneral from "../../../templates/BtnGeneral"
+import { Link } from 'react-router-dom'
+
 const Banner = (props) => {
 	return (
 		<div className="homeBanner d-none d-lg-flex flex-column">
@@ -9,13 +11,15 @@ const Banner = (props) => {
 				Parcourez notre large éventail d'offres en location où à l'achat
 				dans la région que vous souhaitez et prenez rendez-vous ! <br />
 				<br />
-				Vous souhaitez vendre un de vos biens ? 
-                <br />
-                N'hésitez pas à nous
-				contacter pour être mis en relation avec l'un de nos agents.
+				Vous souhaitez vendre un de vos biens ?
+				<br />
+				N'hésitez pas à nous contacter pour être mis en relation avec
+				l'un de nos agents.
 				<br />
 			</p>
-			<BtnGeneral className="BtnBanner " text="Contactez-nous" />
+			<Link to={`/aboutus`} className="LinkBtn" style={{ width: 'fit-content' }}>
+				<BtnGeneral className="BtnBanner" text="Contactez-nous" />
+			</Link>
 		</div>
 	)
 }
