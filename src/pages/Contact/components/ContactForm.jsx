ed29fdf3cef161c1358d03d492474065ea0b51ce
Vincent Mancheron <vmancheron@yahoo.fr>
2022-03-03 14:54:03 +0100

    03/03/22 14:53 Form Contact

--- src/pages/Contact/components/ContactForm.jsx
+++ src/pages/Contact/components/ContactForm.jsx
@@ -1,7 +1,11 @@
-import { useForm } from 'react-hook-form'
+import { set, useForm } from 'react-hook-form'
 import { Button } from 'react-bootstrap'
+import { useState, useContext } from 'react'
+import { Context } from '../../../App'
 
 const ContactForm = () => {
+	const [messageSent, setMessageSent] = useState(false)
+
 	const {
 		register,
 		handleSubmit,
@@ -14,14 +18,21 @@ const ContactForm = () => {
 
 	const { isValid, isSubmitting, isSubmitted, isSubmitSuccessful } = formState
 
+	let isConnected = useContext(Context).connected
+	// let userInfos = useContext(Context).userInfos
+
 	const onSubmit = (data) => {
 		try {
 			let body = JSON.stringify({
+				lastname: data.lastname,
+				firstname: data.firstname,
 				email: data.email,
-				password: data.password,
+				tel: data.tel,
+				subject: data.subject,
+				message: data.message,
 			})
 
-			fetch(process.env.REACT_APP_API_DOMAIN + 'api/user/login', {
+			fetch(process.env.REACT_APP_API_DOMAIN + 'api/user/sendMessage', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json;charset=utf-8',
@@ -33,11 +44,7 @@ const ContactForm = () => {
 				})
 				.then((response) => {
 					if (response.status_code === 200) {
-						localStorage.setItem(
-							'REACT_TOKEN_AUTH_AMAIZON',
-							JSON.stringify(response.token)
-						)
-						window.location.href = process.env.REACT_APP_UI_DOMAIN
+						setMessageSent(true)
 					}
 				})
 		} catch (error) {
@@ -53,83 +60,92 @@ const ContactForm = () => {
 				onSubmit={handleSubmit(onSubmit)}
 				className="w-75"
 			>
-				<div className="d-flex justify-content-around my-3">
-					<input
-						type="text"
-						className={
-							!errors.lastname
-								? 'form-control w-50 me-4 rounded-pill'
-								: 'form-control w-50 me-4 rounded-pill is-invalid'
-						}
-						placeholder="Votre nom"
-						{...register('lastname', {
-							required:
-								'Vous devez indiquer votre nom de famille.',
-						})}
-						onBlur={() => console.log(isValid)}
-					/>
-					{errors.lastname && (
-						<span className="invalid-feedback">
-							{errors.lastname.message}
-						</span>
-					)}
-					<input
-						type="text"
-						className={
-							!errors.firstname
-								? 'form-control w-50 ms-4 rounded-pill'
-								: 'form-control w-50 ms-4 rounded-pill is-invalid'
-						}
-						placeholder="Votre prénom"
-						{...register('firstname', {
-							required: 'Vous devez indiquer votre prénom.',
-						})}
-						onBlur={() => console.log(isValid)}
-					/>
-					{errors.firstname && (
-						<span className="invalid-feedback">
-							{errors.firstname.message}
-						</span>
-					)}
-				</div>
-				<div className="d-flex justify-content-around my-3">
-					<input
-						type="email"
-						className={
-							!errors.email
-								? 'form-control w-50 me-4 rounded-pill'
-								: 'form-control w-50 me-4 rounded-pill is-invalid'
-						}
-						placeholder="Votre email"
-						{...register('email', {
-							required: 'Vous devez indiquer votre email.',
-						})}
-						onBlur={() => console.log(isValid)}
-					/>
-					{errors.email && (
-						<span className="invalid-feedback">
-							{errors.email.message}
-						</span>
-					)}
-					<input
-						type="tel"
-						className={
-							!errors.tel
-								? 'form-control w-50 ms-4 rounded-pill'
-								: 'form-control w-50 ms-4 rounded-pill is-invalid'
-						}
-						placeholder="Votre téléphone"
-						{...register('tel', {
-							required: 'Vous devez indiquer votre téléphone.',
-						})}
-						onBlur={() => console.log(isValid)}
-					/>
-					{errors.tel && (
-						<span className="invalid-feedback">
-							{errors.tel.message}
-						</span>
-					)}
-				</div>
+				{!isConnected ? (
+					<>
+						<div className="d-flex justify-content-around my-3">
+							<input
+								type="text"
+								className={
+									!errors.lastname
+										? 'form-control w-50 me-4 rounded-pill'
+										: 'form-control w-50 me-4 rounded-pill is-invalid'
+								}
+								placeholder="Votre nom"
+								{...register('lastname', {
+									required:
+										'Vous devez indiquer votre nom de famille.',
+								})}
+								onBlur={() => console.log(isValid)}
+							/>
+							{errors.lastname && (
+								<span className="invalid-feedback">
+									{errors.lastname.message}
+								</span>
+							)}
+							<input
+								type="text"
+								className={
+									!errors.firstname
+										? 'form-control w-50 ms-4 rounded-pill'
+										: 'form-control w-50 ms-4 rounded-pill is-invalid'
+								}
+								placeholder="Votre prénom"
+								{...register('firstname', {
+									required:
+										'Vous devez indiquer votre prénom.',
+								})}
+								onBlur={() => console.log(isValid)}
+							/>
+							{errors.firstname && (
+								<span className="invalid-feedback">
+									{errors.firstname.message}
+								</span>
+							)}
+						</div>
+						<div className="d-flex justify-content-around my-3">
+							<input
+								type="email"
+								className={
+									!errors.email
+										? 'form-control w-50 me-4 rounded-pill'
+										: 'form-control w-50 me-4 rounded-pill is-invalid'
+								}
+								placeholder="Votre email"
+								{...register('email', {
+									required:
+										'Vous devez indiquer votre email.',
+								})}
+								onBlur={() => console.log(isValid)}
+							/>
+							{errors.email && (
+								<span className="invalid-feedback">
+									{errors.email.message}
+								</span>
+							)}
+							<input
+								type="tel"
+								className={
+									!errors.tel
+										? 'form-control w-50 ms-4 rounded-pill'
+										: 'form-control w-50 ms-4 rounded-pill is-invalid'
+								}
+								placeholder="Votre téléphone"
+								{...register('tel', {
+									required:
+										'Vous devez indiquer votre téléphone.',
+								})}
+								onBlur={() => console.log(isValid)}
+							/>
+							{errors.tel && (
+								<span className="invalid-feedback">
+									{errors.tel.message}
+								</span>
+							)}
+						</div>
+					</>
+				) : (
+					''
+				)}
 				<div className="my-3">
 					<input
 						type="text"
@@ -160,10 +176,17 @@ const ContactForm = () => {
 						onBlur={() => console.log(isValid)}
 					/>
 				</div>
-				<div className="d-flex justify-content-center my-3">
+				<div className="d-flex justify-content-center align-items-center my-3">
 					<Button type="submit" className="header-btn">
 						Envoyer
 					</Button>
+					{messageSent ? (
+						<span className="ms-3" style={{ color: 'green' }}>
+							Message envoyé !
+						</span>
+					) : (
+						''
+					)}
 				</div>
 			</form>
 		</div>
